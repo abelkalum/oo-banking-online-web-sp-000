69f842d86dfc358e857b2efa97576cfefa12b3eb
Abel Kalum <chumbabel@gmail.com>
2020-05-04 18:35:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,16 +18,16 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
 
     elsif @status == "complete"
       puts "Transaction was already excuted"
     else
-      @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      @sender.balance > @amount 
+      @receiver.deposit == @amount
+      @status = "complete"
     end
   end
 
