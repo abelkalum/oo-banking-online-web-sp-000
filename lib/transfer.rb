75a699eb699d4056eb678bfde0534de82f19d8be
Abel Kalum <chumbabel@gmail.com>
2020-05-04 18:40:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      @status = "rejected"
+      @sender.balance < @amount && @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
