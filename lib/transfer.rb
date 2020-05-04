10adf640fb7eb59ccbfe406a10bebf6c6649010a
Abel Kalum <chumbabel@gmail.com>
2020-05-04 18:56:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,17 +13,15 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
-      @sender.balance < @amount || @sender.account == "closed"
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      if @sender.balance > @amount && @status == "pending" && valid?
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
+      else 
+        @status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      end
     end
-  end
-
   def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount
