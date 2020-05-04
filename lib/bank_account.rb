46ec89800b249ce9271ca45a8bb3a0a6749dfa48
Abel Kalum <chumbabel@gmail.com>
2020-05-04 18:00:05 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
-
+  def initialize
+    @name='Avi'
+    @balance=1000
+    @status=open
+  end
+  
+  def deposit
+    @deposit_amount=1000
+  end
+  
+  def display_balance
+    @display_balance = @balance + @deposit
+  end
+  
+  def valid?
+    if @status=open && @balance>0
+      true
+  end
+  
+  def close_account
+    @status=closed 
+  end
+  
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
-  # your code here
+  def initialize
+    @sender=sender
+    @receiver=receiver
+    @status=pending
+    @transfer_amount=50
+  end
+  
+  def valid?
+    if avid.valid?=true && amanda.valid?=true
+      true
+    else
+      false
+    end
+  end
+  
+  
+      
+  end
+    
 end
